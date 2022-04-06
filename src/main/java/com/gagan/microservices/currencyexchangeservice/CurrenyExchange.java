330514f4c33acc64bc95238708c7b631e869bb56
Gagan Ichake <gaganichake@yahoo.com>
2022-04-05 21:16:07 -0500

    Configure JPA

--- src/main/java/com/gagan/microservices/currencyexchangeservice/CurrenyExchange.java
+++ src/main/java/com/gagan/microservices/currencyexchangeservice/CurrenyExchange.java
@@ -1,10 +1,17 @@
 package com.gagan.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class CurrenyExchange {
+    @Id
     private Long id;
-    private String from;
+    @Column(name = "currency_from")
+    private String from; // since "from" is a reserved key word in SQL
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
