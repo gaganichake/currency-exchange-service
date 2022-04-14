67b4861a8b649aeb36ba317691bafb1b72e1d885
Gagan Ichake <gaganichake@yahoo.com>
2022-04-14 08:12:34 -0500

    Connecting Microservice with Zipkin

--- src/main/java/com/gagan/microservices/currencyexchangeservice/CurrenyExchangeController.java
+++ src/main/java/com/gagan/microservices/currencyexchangeservice/CurrenyExchangeController.java
@@ -1,5 +1,7 @@
 package com.gagan.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrenyExchangeController {
 
+    private final Logger logger = LoggerFactory.getLogger(CurrenyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository repository;
 
@@ -17,6 +21,8 @@ public class CurrenyExchangeController {
 
     @GetMapping("/curreny-exchange/from/{from}/to/{to}")
     public CurrenyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         CurrenyExchange currenyExchange = repository.findByFromAndTo(from, to);
         if(currenyExchange == null) {
             throw new RuntimeException("Unable to find data for " + from + " to " + to);
